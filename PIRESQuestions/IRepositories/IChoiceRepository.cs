a65df9285b0976b3a5013a1734c2aa6c3c30643b
Valentin Furcy <v.furcy@laposte.net>
2024-01-24 16:59:35 +0100

    Fixed: IRepositories => Choice/Question

--- PIRESQuestions/IRepositories/IChoiceRepository.cs
+++ PIRESQuestions/IRepositories/IChoiceRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace IRepositories
 {
     public interface IChoiceRepository
     {
+        Task<Choice> CreateChoiceAsync(Choice choice);
+        Task<Choice> UpdateChoiceAsync(Choice choice);
+        Task<Choice> DeleteChoiceAsync(int id);
+        Task<Choice> GetChoiceByIdAsync(int id);
+        Task<List<Choice>> GetAllChoiceAsync();
     }
 }
--- PIRESQuestions/IRepositories/IQuestionRepository.cs
+++ PIRESQuestions/IRepositories/IQuestionRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace IRepositories
 {
     public interface IQuestionRepository
     {
+        Task<Question> CreateQuestionAsync(Question question);
+        Task<Question> UpdateQuestionAsync(Question question);
+        Task<Question> DeleteQuestionAsync(int id);
+        Task<Question> GetQuestionByIdAsync(int id);
+        Task<List<Question>> GetAllQuestionsAsync();
     }
 }
