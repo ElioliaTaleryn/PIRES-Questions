37eb795a9823470a067f70cd608ea6106a76ca3f
Valentin Furcy <v.furcy@laposte.net>
2024-01-24 15:45:05 +0100

    Add: Controllers => Question/Choice/TimerCD

---
+++ PIRESQuestions/PIRESQuestions/Controllers/ChoiceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PIRESQuestions.Controllers
+{
+    public class ChoiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
