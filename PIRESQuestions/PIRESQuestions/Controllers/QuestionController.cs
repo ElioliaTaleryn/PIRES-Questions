b3d500002a9e5679264fc260cefdd8887a58cc79
valentin.f <v.furcy@laposte.net>
2024-01-25 12:19:02 +0100

    Fixed: CreateQuestion GET/POST

--- PIRESQuestions/PIRESQuestions/Controllers/QuestionController.cs
+++ PIRESQuestions/PIRESQuestions/Controllers/QuestionController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entities;
+using IServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PIRESQuestions.Controllers
 {
     public class QuestionController : Controller
     {
+        IQuestionService QuestionService { get; }
+        public QuestionController(IQuestionService questionService)
+        {
+            QuestionService = questionService;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateQuestion()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateQuestion(Question question)
+        {
+            if(!ModelState.IsValid && string.IsNullOrEmpty(question.Label) && question.FormId == null ) 
+            {
+                ModelState.AddModelError("Label", "Le champ Label est obligatoire.");
+                ModelState.AddModelError("FormId", "La question doit appartenir à un formulaire");
+           
+                return View(question);
+            }
+            else
+            {
+                question = await QuestionService.CreateQuestionAsync(question);
+
+                return View("Index");
+            }
         }
     }
 }
