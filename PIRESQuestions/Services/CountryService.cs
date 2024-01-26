7ece576ccdd58858c731edc992252e57518dc20a
Elodie Tribout <tribout.elodie@gmail.com>
2024-01-26 16:34:29 +0100

    Fix: message d'exception + champs inutiles

--- PIRESQuestions/Services/CountryService.cs
+++ PIRESQuestions/Services/CountryService.cs
@@ -2,29 +2,24 @@
 using IRepositories;
 using IServices;
 using Repositories.Entity_Framework;
-using Repositories.Repositories;
 using Services.Exceptions;
-using System.Reflection;
 
 namespace Services
 {
-    public class CountryService(ICountryRepository _countryRepository, ApplicationDbContext _context) : ICountryService
+    public class CountryService(ICountryRepository countryRepository, ApplicationDbContext context) : ICountryService
     {
-        private readonly ICountryRepository countryRepository = _countryRepository;
-        private readonly ApplicationDbContext context = _context;
-
         public async Task<Country> CreateCountryAsync(Country country)
         {
             if (!context.Countries.Contains(country))
             {
-                throw new CountryServiceException($"Country name value invalid: doesn't exists in BDD.");
+                throw new CountryServiceException($"Country name value invalid: doesn't exists in DB.");
             }
             return await countryRepository.CreateCountryAsync(country);
         }
 
         public async Task<bool> DeleteCountryAsync(Country country)
         {
-            return await _countryRepository.DeleteCountryAsync(country);
+            return await countryRepository.DeleteCountryAsync(country);
         }
 
         public async Task<IEnumerable<Country>> GetAllCountriesAsync()
--- PIRESQuestions/Services/GenderService.cs
+++ PIRESQuestions/Services/GenderService.cs
@@ -6,23 +6,20 @@ using Services.Exceptions;
 
 namespace Services
 {
-    public class GenderService(ApplicationDbContext _context, IGenderRepository _genderRepository) : IGenderService
+    public class GenderService(ApplicationDbContext context, IGenderRepository genderRepository) : IGenderService
     {
-        private readonly ApplicationDbContext context = _context;
-        private readonly IGenderRepository genderRepository = _genderRepository;
-
         public async Task<Gender> CreateGenderAsync(Gender gender)
         {
             if (!context.Genders.Contains(gender))
             {
-                throw new GenderServiceException($"Gender label value invalid: doesn't exists in BDD.");
+                throw new GenderServiceException($"Gender label value invalid: doesn't exists in DB.");
             }
             return await genderRepository.CreateGenderAsync(gender);
         }
 
         public async Task<bool> DeleteGenderAsync(Gender gender)
         {
-            return await _genderRepository.DeleteGenderAsync(gender);
+            return await genderRepository.DeleteGenderAsync(gender);
         }
 
         public async Task<IEnumerable<Gender>> GetAllGendersAsync()
