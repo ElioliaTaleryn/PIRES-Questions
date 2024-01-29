5941aac071b8469e5829472c3681add4292f1d11
Elodie Tribout <tribout.elodie@gmail.com>
2024-01-29 15:03:47 +0100

    Add: Anonymous et answer entities

---
+++ PIRESQuestions/Entities/Answer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Answer
+    {
+        #region Fields
+        public int Id { get; set; }
+        [Required]
+        public required DateTime Horodatage { get; set; }
+        #endregion
+        #region Relative Fields
+        [Required]
+        public required int QuestionId { get; set; }
+        public Question Question { get; set; }
+        [Required]
+        public required int ChoiceId { get; set; }
+        public Choice Choice { get; set; }
+        [Required]
+        public required int AnonymousId { get; set; }
+        public Anonymous Anonymous { get; set; }
+        #endregion
+    }
+}
