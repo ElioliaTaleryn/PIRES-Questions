94bc4d892638c0b793eef5144f1f81c536ffe425
valentin.f <v.furcy@laposte.net>
2024-01-29 11:16:06 +0100

    Fixed: ajout CreateQuestionWithChoiceAsync()

--- PIRESQuestions/Services/QuestionService.cs
+++ PIRESQuestions/Services/QuestionService.cs
@@ -6,46 +6,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ViewModels;
 
 namespace Services
 {
     public class QuestionService : IQuestionService
     {
-        IQuestionRepository QuestionRepository;
-
+        IQuestionRepository _questionRepository;
         public QuestionService(IQuestionRepository questionRepository)
         {
-            QuestionRepository = questionRepository;
+            _questionRepository = questionRepository;
         }
 
         public async Task<Question> CreateQuestionAsync(Question question)
         {
-            return await QuestionRepository.CreateQuestionAsync(question);
-
-            //if (question.Label != null && question.FormId != null)
-            //{
-            //    return await QuestionRepository.CreateQuestionAsync(question);
-            //}
-            //else
-            //{
-            //    string erreur = "";
-            //    if (question.Label == null)
-            //    {
-            //        erreur = "Veuillez renseigner la question que vous souhaitez poser";
-            //    }
-            //    else if (question.FormId == null)
-            //    { 
-            //        erreur = "La question n'est assignée à aucun formulaire existant"; 
-            //    }
-            //    throw new Exception(erreur);
-            //}
+            return await _questionRepository.CreateQuestionAsync(question);
         }
-
+  
+        public async Task<Question> CreateQuestionWithChoiceAsync(Question question)
+        {
+            var questionToCreate = new Question()
+            {
+                Label = question.Label,
+                Description = question.Description,
+                FormId = question.FormId,
+                Choices = question.Choices.Select(question => new Choice
+                {
+                    Label = question.Label,
+                    QuestionId = question.QuestionId,
+                }).ToList()
+            };
+            return await _questionRepository.CreateQuestionWithChoiceAsync(question);
+        }
         public async Task<Question> UpdateQuestionAsync(Question question)
         {
             if (question.Label != null && question.FormId != null)
             {
-                return await QuestionRepository.UpdateQuestionAsync(question);
+                return await _questionRepository.UpdateQuestionAsync(question);
             }
             else
             {
@@ -66,7 +63,7 @@ namespace Services
         {
             if (id != null)
             {
-                await QuestionRepository.DeleteQuestionAsync(id);
+                await _questionRepository.DeleteQuestionAsync(id);
             }
             else throw new Exception("Aucune valeur permettant la suppression de la question");
         }
@@ -75,20 +72,20 @@ namespace Services
         {
             if (id != null)
             {
-                return await QuestionRepository.GetQuestionByIdAsync(id);
+                return await _questionRepository.GetQuestionByIdAsync(id);
             }
             else throw new Exception("Aucune valeur permettant de rechercher la question");
         }
 
         public async Task<List<Question>> GetAllQuestionsAsync()
         {
-            return await QuestionRepository.GetAllQuestionsAsync();
+            return await _questionRepository.GetAllQuestionsAsync();
         }
         public async Task<List<Question>> GetQuestionByFormIdAsync(int formId)
         {
             if(formId != null) 
             {
-                return await QuestionRepository.GetQuestionByFormIdAsync(formId);
+                return await _questionRepository.GetQuestionByFormIdAsync(formId);
             }
             else throw new Exception("Aucune valeur permettant de rechercher les questions");
         }
