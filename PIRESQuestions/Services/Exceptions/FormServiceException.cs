355d84b3d13cc2eb8753f610a89a68ed0962718e
guilhèm blanchard <github@gblanchard.eu>
2024-01-29 16:25:31 +0100

    Add: Form CRUD

---
+++ PIRESQuestions/Services/Exceptions/FormServiceException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace Services.Exceptions
+{
+    [Serializable]
+    internal class FormServiceException : Exception
+    {
+        public FormServiceException() 
+        { 
+        }
+
+        public FormServiceException(string? message) : base(message)
+        {
+        }
+
+        public FormServiceException(string? message, Exception? innerException) : base(message, innerException) 
+        {
+        }
+
+        protected FormServiceException(SerializationInfo info, StreamingContext context) : base(info, context) 
+        {
+        }
+    }
+}
