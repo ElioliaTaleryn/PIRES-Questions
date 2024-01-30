66ad2cd3f3bac113f8ff034b0cb9a9ce4a7a2080
valentin.f <v.furcy@laposte.net>
2024-01-30 11:35:03 +0100

    Fixed: rename appContext => _appContext

--- PIRESQuestions/Repositories/Repositories/ChoiceRepository.cs
+++ PIRESQuestions/Repositories/Repositories/ChoiceRepository.cs
@@ -12,26 +12,26 @@ namespace Repositories.Repositories
 {
     public class ChoiceRepository : IChoiceRepository
     {
-        ApplicationDbContext appContext;
+        ApplicationDbContext _appContext;
         public ChoiceRepository(ApplicationDbContext applicationDbContext)
         {
-            appContext = applicationDbContext;
+            _appContext = applicationDbContext;
         }
 
         public async Task<Choice> CreateChoiceAsync(Choice choice)
         {
-            appContext.Choices.Add(choice);
-            await appContext.SaveChangesAsync();
+            _appContext.Choices.Add(choice);
+            await _appContext.SaveChangesAsync();
             return choice;
         }
 
         public async Task<bool> DeleteChoiceAsync(int id)
         {
-            var choice = appContext.Choices.FirstOrDefault(c => c.Id == id);
+            var choice = _appContext.Choices.FirstOrDefault(c => c.Id == id);
             if (choice != null)
             {
-                appContext.Choices.Remove(choice);
-                await appContext.SaveChangesAsync();
+                _appContext.Choices.Remove(choice);
+                await _appContext.SaveChangesAsync();
                 return true;
             }
             else return false;
@@ -40,7 +40,7 @@ namespace Repositories.Repositories
         public async Task<List<Choice>> GetAllChoiceAsync()
         {
             List<Choice> listChoices = new List<Choice>();
-            listChoices = await appContext.Choices.ToListAsync();
+            listChoices = await _appContext.Choices.ToListAsync();
             if (listChoices.Any())
             {
                 return listChoices;
@@ -50,7 +50,7 @@ namespace Repositories.Repositories
 
         public async Task<Choice> GetChoiceByIdAsync(int id)
         {
-            Choice choice = await appContext.Choices.FindAsync(id);
+            Choice choice = await _appContext.Choices.FindAsync(id);
             if (choice != null)
             {
                 return choice;
@@ -60,12 +60,12 @@ namespace Repositories.Repositories
 
         public async Task<Choice> UpdateChoiceAsync(Choice choice)
         {
-            Choice choiceUpdate = appContext.Choices.FirstOrDefault(c => c.Id == choice.Id);
+            Choice choiceUpdate = _appContext.Choices.FirstOrDefault(c => c.Id == choice.Id);
             if (choiceUpdate != null) 
             {
                 choiceUpdate.Label = choice.Label;
                 choiceUpdate.QuestionId = choice.QuestionId;
-                await appContext.SaveChangesAsync();
+                await _appContext.SaveChangesAsync();
                 return choice;
             }
             else
